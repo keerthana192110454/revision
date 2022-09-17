e916f224846424be9afc031796f9f03ab406d487
keerthana192110454 <112535982+keerthana192110454@users.noreply.github.com>
2022-09-17 16:36:20 +0530

    Add files via upload
    sample.txt,a.txt
    sample1.txt
    s123.txt,a.txt
    s@12.txt,a.txt
    sample S.txt,a.txt

---
+++ day5 merging.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	FILE *a,*b,*c;
+	char list[100];
+	a=fopen("D:\reading.txt","r");
+	b=fopen("D:\exam.txt","r");
+	c=fopen("D:\EXAM READING.txt","w");
+
+	while (c!= EOF)
+	{
+		fputc(f*c,f*b;
+		c=fgetc(f*a);
+	}
+	printf("contents merged sucessfully");
+}
